94f3538dc6a91301ab5f0e83cb4b41e71ef43344
Alexandru Stefan Andries <20018146@studenti.uniupo.it>
2020-05-21 13:50:50 +0200

    Exercise 1.11

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({text, handleClick}) =>(
+  <div>
+    <button onClick={handleClick}>{text}</button>  
+  </div>
+)
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+  const randomizeAnecdote = () =>{
+    const min = 0
+    const max = anecdotes.length
+    const rand = Math.floor(min + Math.random() * (max - min))
+    console.log(rand)
+    setSelected(rand)
+  }
+  return (
+    <div>
+      {props.anecdotes[selected]}
+      <Button text='Random quote' handleClick = {randomizeAnecdote}/>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
