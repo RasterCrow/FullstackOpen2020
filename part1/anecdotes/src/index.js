1eb5570e5ea74aef348291b90bacf19d72f23d42
Alexandru Stefan Andries <20018146@studenti.uniupo.it>
2020-05-21 14:27:52 +0200

    Exercise 1.14 - End of part1

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,15 +1,49 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Title = ({text}) => <h1>{text}</h1>
+
 const Button = ({text, handleClick}) =>(
   <div>
     <button onClick={handleClick}>{text}</button>  
   </div>
 )
+
+const BestAnecdote = ({votes,anecdotes}) => {
+  let max=0
+  let indexMostVoted = 0
+
+  //checks for the one with most votes, if more than one takes the first itfind
+  for (let i=0; i<votes.length; i++) {
+    console.log(max)
+    if(votes[i]>max){
+      max=votes[i]
+      indexMostVoted=i
+    }
+  }
+  //if all have 0 votes, it display a custom message
+  if(max === 0){
+    return(
+      <div>
+      <Title text='Anecdote with most votes'/>
+        No anecdote has been voted today.
+      </div>
+    )
+    //otherwise displays the one with most votes found
+  }else{
+    return(
+      <div>
+      <Title text='Anecdote with most votes'/>
+      {anecdotes[indexMostVoted]}
+      </div>
+    )
+  }
+}
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [votes, setAll] = useState(Array(anecdotes.length).fill(0))
   
+  //functions that takes a random number between 0 and anecdotes.length excluded
   const randomizeAnecdote = () =>{
     const min = 0
     const max = anecdotes.length
@@ -17,6 +51,7 @@ const App = (props) => {
     setSelected(rand)
   }
   
+  //when you vote for an anecdote, it updates the votes array using a copy of it
   const voteForQuote = () =>{
     //create copy of array
     const votesCopy = [...votes]
@@ -25,10 +60,14 @@ const App = (props) => {
   }
   return (
     <div>
+      <Title text='Anecdote of the day'/>
       {props.anecdotes[selected]}
       <p>has {votes[selected]} votes</p>
       <Button text='Vote' handleClick = {voteForQuote}/>
       <Button text='Random quote' handleClick = {randomizeAnecdote}/>
+      
+      <BestAnecdote votes={votes} anecdotes={props.anecdotes}/>
+      
       
     </div>
   )
