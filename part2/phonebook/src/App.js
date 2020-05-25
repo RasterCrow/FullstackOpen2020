7d3189a89042cb65e6694a48e230a55fb7908451
Alexandru Stefan Andries <20018146@studenti.uniupo.it>
2020-05-25 19:33:51 +0200

    Exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,24 @@
 import React, { useState } from 'react'
 
 
-const Numbers = ({numbers}) => {
+const Persons = ({persons}) => {
   return (
-    numbers.map(n => <li key={n.name}>{n.name} {n.phone}</li>)
+    persons.map(n => <li key={n.name}>{n.name} {n.phone}</li>)
   )
 }
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { 
-      name: 'Arto Hellas',
-      phone:'040-1234567'
-    }
+    { name: 'Arto Hellas', phone: '040-123456' },
+    { name: 'Ada Lovelace', phone: '39-44-5323523' },
+    { name: 'Dan Abramov', phone: '12-43-234345' },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone ] = useState('')
+  const [ personFilter, setPersonFilter ] = useState({
+    active: false, filter:''
+  })
 
   const addPerson = (event)=>{
     event.preventDefault()
@@ -31,7 +34,14 @@ const App = () => {
       setNewName('')
     }
   }
-
+ 
+  console.log('Filter Active :',personFilter.active)
+  const personsToLoad = personFilter.active
+    ? persons.filter((element) => {
+        return element.name.toLowerCase().includes(personFilter.filter.toLowerCase())
+      })
+    : persons
+    
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
@@ -40,10 +50,18 @@ const App = () => {
     console.log(event.target.value)
     setNewPhone(event.target.value)
   }
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setPersonFilter({
+      active : true, filter : event.target.value
+    })
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter person with: <input onChange={handleFilterChange} /></div>
+      <h2>Add Person</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleNameChange}  /></div>
         <div>number: <input value={newPhone} onChange={handlePhoneChange} /></div>
@@ -52,7 +70,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers numbers={persons}/>
+      <Persons persons={personsToLoad}/>
     </div>
   )
 }
