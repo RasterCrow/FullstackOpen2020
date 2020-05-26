d761ef6059d29b4af5d74363b81617126edd614c
Alexandru Stefan Andries <20018146@studenti.uniupo.it>
2020-05-26 21:32:31 +0200

    Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PersonAdd from './components/PersonAdd'
 import PersonsList from './components/PersonsList'
 import PersonsFilter from './components/PersonsFilter'
-
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', phone: '040-123456' },
-    { name: 'Ada Lovelace', phone: '39-44-5323523' },
-    { name: 'Dan Abramov', phone: '12-43-234345' },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
+  const hook = () =>{
+    
+      console.log('effect');
+      axios
+        .get('http://localhost:3001/persons')
+        .then(response =>{
+          console.log('got response');
+          setPersons(response.data)
+  
+        })
+  
+    }
+  useEffect(hook,[]);
 
   //In the filter component i just update the personFilter values, but the actual filtering (change data to load)
   //its done here inside the App.js, since  I need to to use the data after filtering in other components. I couldnt update more components at once.
