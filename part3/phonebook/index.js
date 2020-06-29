98dfc81243cad21a9980bc5ce50950857f23b2a8
Alex <crow@crow-nest.station>
2020-06-29 17:27:53 +0200

    part3 exercise 3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -13,6 +13,13 @@ app.get("/api/persons",(req,res)=>{
     res.json(db)
 })
 
+app.get("/info",(req,res)=>{
+    let nPeople = db["persons"].length
+    let date = new Date();
+    let message = `Phonebook has info on ${nPeople} people. \n ${date}`
+    res.send(message)
+})
+
 const PORT = 3001
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`)
