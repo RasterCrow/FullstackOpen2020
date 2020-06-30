5245020d670a596c926d59d759f1888d382bde82
Alex Andries <rastercrow@gmail.com>
2020-06-30 18:21:38 +0200

    Exercise 3.11

---
+++ part3/phonebook/server/build/precache-manifest.5956ad66b04a13ba0a87e289fdd7e91a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4ba1280d3c9f60414d474a69f1e2a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "517d41be379d4e58177f",
+    "url": "/static/css/main.6eddf537.chunk.css"
+  },
+  {
+    "revision": "e690b7896d4bc35c0816",
+    "url": "/static/js/2.ddec9da4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ddec9da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "517d41be379d4e58177f",
+    "url": "/static/js/main.ef7e9e26.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
